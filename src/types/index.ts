cd3f27c19754575a9de4b22b4d7442ed5d8594fe
Mizu <liamash3@gmail.com>
2025-10-22 16:21:08 +1100

    add animagine_xl image model

--- src/types/index.ts
+++ src/types/index.ts
@@ -12,6 +12,8 @@ export interface ModelConfiguration {
     checkpointName: string;
     vae: string;
     steps: number;
+    cfg?: number;
+    sampler_name?: string;
     imageHeight: number;
     imageWidth: number;
     defaultPositivePrompt: string;
@@ -19,7 +21,68 @@ export interface ModelConfiguration {
 }
 
 export interface WorkflowData {
-    [key: string]: any;
+    KSampler: KSamplerNode;
+    Checkpoint: CheckpointNode;
+    EmptyLatentImage: EmptyLatentImageNode;
+    PositivePrompt: PromptNode;
+    NegativePrompt: PromptNode;
+    VAEDecode: VAEDecodeNode;
+    SaveImageWebsocket: SaveImageWebsocketNode;
+    VAELoader: VAELoaderNode;
+}
+
+interface BaseNode<T> {
+    inputs: T;
+    class_type: string;
+}
+
+interface KSamplerNode extends BaseNode<KSamplerInputs> {}
+interface CheckpointNode extends BaseNode<CheckpointInputs> {}
+interface EmptyLatentImageNode extends BaseNode<EmptyLatentImageInputs> {}
+interface PromptNode extends BaseNode<PromptInputs> {}
+interface VAEDecodeNode extends BaseNode<VAEDecodeInputs> {}
+interface SaveImageWebsocketNode extends BaseNode<SaveImageWebsocketInputs> {}
+interface VAELoaderNode extends BaseNode<VAELoaderInputs> {}
+
+interface KSamplerInputs {
+    seed: number;
+    steps: number;
+    cfg: number;
+    sampler_name: string;
+    scheduler: string;
+    denoise: number;
+    model: [string, number];
+    positive: [string, number];
+    negative: [string, number];
+    latent_image: [string, number];
+}
+
+interface CheckpointInputs {
+    ckpt_name: string;
+}
+
+interface EmptyLatentImageInputs {
+    width: number;
+    height: number;
+    batch_size: number;
+}
+
+interface PromptInputs {
+    text: string;
+    clip: [string, number];
+}
+
+interface VAEDecodeInputs {
+    samples: [string, number];
+    vae: [string, number];
+}
+
+interface SaveImageWebsocketInputs {
+    images: [string, number];
+}
+
+interface VAELoaderInputs {
+    vae_name: string;
 }
 
 export interface PromptData {
